ff41d52a4914a3ed52cda51afb25d8267b455560
vt <trachenko.v.a@gmail.com>
2024-07-11 16:38:50 +0700

    feat: migrate header to function component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './header.module.css';
 
 import { getQueryFromLS, saveNewQueryInLS } from '../../utils/localStorage';
@@ -7,88 +7,46 @@ import { Product } from '../../models/product';
 import { getProductsFromApi } from '../../services/api';
 import { TEXTS } from '../../texts';
 
-type HeaderProps = {
+interface HeaderProps {
   setProducts: (data: Product[]) => void;
   setIsLoading: (isLoading: boolean) => void;
   isLoading: boolean;
-};
+}
 
-type HeaderState = {
-  query: string;
-  hasError: boolean;
-};
+export function Header({ setProducts, setIsLoading, isLoading }: HeaderProps) {
+  const [query, setQuery] = useState(getQueryFromLS());
+  const [hasError, setHasError] = useState(false);
 
-export class Header extends React.Component<HeaderProps, HeaderState> {
-  constructor(props: HeaderProps) {
-    super(props);
-    this.state = {
-      query: '',
-      hasError: false,
-    };
-  }
+  if (hasError) throw new Error(TEXTS.ERROR_TEXT);
 
-  componentDidMount() {
-    const queryInLS = getQueryFromLS();
-    this.setState({
-      query: queryInLS,
-    });
-    this.getData(queryInLS);
-  }
-
-  handleClickFind = async () => {
-    const { query } = this.state;
-    const queryInLS = getQueryFromLS();
-    const trimmedQuery = query.trim();
-    if (queryInLS !== trimmedQuery) {
-      this.getData(trimmedQuery);
-    }
-  };
-
-  handleQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({ query: event.target.value });
-  };
-
-  handleClickError = () => {
-    this.setState({ hasError: true });
-  };
-
-  getData(query: string) {
-    const { setProducts, setIsLoading } = this.props;
+  async function handleClickFind() {
     setIsLoading(true);
     saveNewQueryInLS(query);
-    getProductsFromApi(query).then((data) => {
-      setProducts(data.products);
-      setIsLoading(false);
-    });
+    const { products } = await getProductsFromApi(query);
+    setProducts(products);
+    setIsLoading(false);
   }
 
-  render() {
-    const { query, hasError } = this.state;
-    const { isLoading } = this.props;
+  useEffect(() => {
+    handleClickFind();
+  }, []);
 
-    if (hasError) throw new Error(TEXTS.ERROR_TEXT);
+  return (
+    <header className={styles.header}>
+      <input
+        type="text"
+        className={styles.findInput}
+        placeholder={TEXTS.INPUT_PLACEHOLDER}
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <button type="button" onClick={handleClickFind} disabled={isLoading}>
+        {isLoading ? TEXTS.LOADING : TEXTS.BUTTON_FIND}
+      </button>
 
-    return (
-      <header className={styles.header}>
-        <input
-          type="text"
-          className={styles.findInput}
-          placeholder={TEXTS.INPUT_PLACEHOLDER}
-          value={query}
-          onChange={this.handleQueryChange}
-        />
-        <button
-          type="button"
-          onClick={this.handleClickFind}
-          disabled={isLoading}
-        >
-          {isLoading ? TEXTS.LOADING : TEXTS.BUTTON_FIND}
-        </button>
-
-        <button type="button" onClick={this.handleClickError}>
-          {TEXTS.BUTTON_ERROR}
-        </button>
-      </header>
-    );
-  }
+      <button type="button" onClick={() => setHasError(true)}>
+        {TEXTS.BUTTON_ERROR}
+      </button>
+    </header>
+  );
 }
