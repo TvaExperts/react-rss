a25f334c55f5d3224b6aa6ab27a5bd5b1026f971
vt <trachenko.v.a@gmail.com>
2024-07-31 00:08:19 +0700

    chore: setup next js

--- src/router/routes.ts
+++
@@ -1,4 +0,0 @@
-export const ROUTES = {
-  HOME: '/',
-  PRODUCT: '/product',
-} as const;
--- src/tests/mocks/mockOneProduct.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from '../../models/product';
-
-export const mockOneProduct: Product = {
-  id: 11,
-  title: 'perfume Oil',
-  description:
-    'Mega Discount, Impression of Acqua Di Gio by GiorgioArmani concentrated attar perfume Oil',
-  images: ['https://i.dummyjson.com/data/products/11/1.jpg'],
-};
